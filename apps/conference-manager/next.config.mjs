96cf73fb63b1b98635e987dafd60810cada02371
maciejas22 <motorosas2012@gmail.com>
2024-08-19 20:45:50 +0200

    chore: fix deployment

--- apps/conference-manager/next.config.mjs
+++ apps/conference-manager/next.config.mjs
@@ -3,6 +3,22 @@ import withBundleAnalyzer from '@next/bundle-analyzer';
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/graphql',
+        destination: process.env.NEXT_PUBLIC_API_URL,
+      },
+    ];
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   experimental: {
     turbo: {
       rules: {
@@ -12,14 +28,6 @@ const nextConfig = {
         },
       },
     },
-  },
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/graphql',
-        destination: process.env.NEXT_PUBLIC_API_URL,
-      },
-    ];
   },
 };
 
