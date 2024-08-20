4b1c7771cee8260c59d5bedd4b5af9a0bda541a6
maciejas22 <motorosas2012@gmail.com>
2024-08-20 21:37:26 +0200

    chore: update env

--- packages/config/env/client.ts
+++ packages/config/env/client.ts
@@ -4,7 +4,9 @@ const envClientSchema = z.object({
   NEXT_PUBLIC_UI_GQL_PROXY_URL: z.string(),
 });
 
-const envClient = envClientSchema.safeParse(process.env);
+const envClient = envClientSchema.safeParse({
+  NEXT_PUBLIC_UI_GQL_PROXY_URL: process.env.NEXT_PUBLIC_UI_GQL_PROXY_URL,
+});
 
 if (!envClient.success) {
   throw new Error('Invalid client environment variables');
