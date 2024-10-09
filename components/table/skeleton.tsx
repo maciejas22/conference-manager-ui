a81d1e4eb14454df9b0bb40000ae93722c499b9c
maciejas22 <motorosas2012@gmail.com>
2024-10-09 22:00:53 +0200

    feature: add table skeleton loaders

---
+++ components/table/skeleton.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { Skeleton } from '@nextui-org/skeleton';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from '@nextui-org/table';
+
+type TableSkeletonLoaderProps = {
+  rows?: number;
+  columns?: number;
+};
+
+export function TableSkeletonLoader({
+  columns = 4,
+  rows = 5,
+}: TableSkeletonLoaderProps) {
+  return (
+    <Table aria-label="table skeleton loader">
+      <TableHeader>
+        {Array.from({ length: columns }).map((_, index) => (
+          <TableColumn key={index}>{''}</TableColumn>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {Array.from({ length: rows }).map((_, index) => (
+          <TableRow key={index}>
+            {Array.from({ length: columns }).map((_, index) => (
+              <TableCell key={index}>
+                <Skeleton className="w-full h-6 rounded-xl" />
+              </TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
